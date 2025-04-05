b04c0c6f0239aca2e4cb724e2b1f7b2cbf55403a
SK3620 <k.n.t11193620@gmail.com>
2025-04-05 21:55:20 +0900

    Initial Commit

---
+++ Sample-VIPER-ToDo-App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Sample-VIPER-ToDo-App
+//
+//  Created by 鈴木 健太 on 2025/04/05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
